d4404246b63cf8336cb95633bee311fc351da20d
yixiWang <1530711658@qq.com>
2019-12-28 11:19:19 +0800

    猫眼电影(排版)

--- data1/src/App.vue
+++ data1/src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
+//   font-size: calc(100 / 750 * 100vw);
+
+// <router-link to="/">Home</router-link> |
+// <router-link to="/about">About</router-link>
+#app {
+  width: 100%;
+  height: 100%;
+  background: #ccc;
+  display: flex;
+  flex-direction: column;
+}
+// header{
+//   width: 100%;
+//   height: 50px;
+//   background: red;
+// }
 #nav {
-  padding: 30px;
+  // flex: 1;
+  // padding: 30px;
 
   a {
     font-weight: bold;
--- data1/src/views/About.vue
+++ data1/src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <header>猫眼电影</header>
+    <div id="nav">nav</div>
   </div>
 </template>
+<style lang="scss">
+@import url("../../public/css/reset.scss");
+body {
+  // width: 100%;
+  // height: 100%;
+  // background: #ccc;
+  font-size: calc(100 / 750 * 100vw);
+}
+.about {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+  background: #ccc;
+}
+header {
+  width: 100%;
+  height: 50px;
+  background: red;
+  text-align: center;
+  color: #fff;
+  font-size: 20px;
+  line-height: 50px;
+  font-weight: 500;
+}
+#nav {
+  flex: 1;
+  overflow: auto;
+  background: lavender;
+}
+</style>
